acf169c1e4001fb22ee0732a0e2be0eb36eb9b9d
MarlenaFlueh <mf@esyoil.com>
2018-10-08 09:42:05 +0200

    add netflix icon, add app icon

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,7 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 
@@ -21,35 +22,50 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
 			HBox root = new HBox();
 			root.getStyleClass().add("root");
-			root.setPrefSize(340, 7);
+			root.setPrefSize(410, 7);
 			root.setSpacing(5);
 			root.setAlignment(Pos.CENTER);
 			
+			Button btnZero = new Button();
 			Button btnOne = new Button();
 			Button btnTwo = new Button();
 			Button btnThree = new Button();
 			Button btnFour = new Button();
 			Button btnFive = new Button();
 			
+			btnZero.setPrefSize(64, 64);
 			btnOne.setPrefSize(64, 64);
 			btnTwo.setPrefSize(64, 64);
 			btnThree.setPrefSize(64, 64);
 			btnFour.setPrefSize(64, 64);
 			btnFive.setPrefSize(64, 64);
 			
+			btnZero.getStyleClass().add("btn0");
 			btnOne.getStyleClass().add("btn1");
 			btnTwo.getStyleClass().add("btn2");
 			btnThree.getStyleClass().add("btn3");
 			btnFour.getStyleClass().add("btn4");
 			btnFive.getStyleClass().add("btn5");
 			
+			root.getChildren().add(btnZero);
 			root.getChildren().add(btnOne);
 			root.getChildren().add(btnTwo);
 			root.getChildren().add(btnThree);
 			root.getChildren().add(btnFour);
 			root.getChildren().add(btnFive);
+			
+			btnZero.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent arg0) {
+					// TODO Auto-generated method stub
+					getHostServices().showDocument("https://www.netflix.com/");
+				}
+				
+			});
 			
 			btnOne.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -86,7 +102,7 @@ public class Main extends Application {
 				@Override
 				public void handle(ActionEvent arg0) {
 					// TODO Auto-generated method stub
-					getHostServices().showDocument("http://rapsberrypros.com");
+					getHostServices().showDocument("http://raspberrypros.com");
 				}
 				
 			});
@@ -101,11 +117,12 @@ public class Main extends Application {
 				
 			});
 			
-			Scene scene = new Scene(root,400,75);
+			Scene scene = new Scene(root,470,75);
 			scene.setFill(Color.TRANSPARENT);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setX((screenSize.getWidth() / 2) - 100);
+			primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("../images/raspberry.png")));;
+			primaryStage.setX((screenSize.getWidth() / 2) - 235);
 			primaryStage.setY(20);
 			primaryStage.initStyle(StageStyle.TRANSPARENT);
 			primaryStage.show();
